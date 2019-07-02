e3bbc3e12b816b6fae2cec9e50dc25f30c75fd50
Urip Subekti <urip.subekti13@gmail.com>
2019-07-02 20:10:46 +0700

    init

---
+++ build/Release/.deps/Release/obj.target/node_printer/src/node_printer.o.d
@@ -0,0 +1,20 @@
+cmd_Release/obj.target/node_printer/src/node_printer.o := c++ '-DNODE_GYP_MODULE_NAME=node_printer' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/subekti/.node-gyp/10.15.1/include/node -I/Users/subekti/.node-gyp/10.15.1/src -I/Users/subekti/.node-gyp/10.15.1/deps/openssl/config -I/Users/subekti/.node-gyp/10.15.1/deps/openssl/openssl/include -I/Users/subekti/.node-gyp/10.15.1/deps/uv/include -I/Users/subekti/.node-gyp/10.15.1/deps/zlib -I/Users/subekti/.node-gyp/10.15.1/deps/v8/include  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/node_printer/src/node_printer.o.d.raw   -c -o Release/obj.target/node_printer/src/node_printer.o ../src/node_printer.cc
+Release/obj.target/node_printer/src/node_printer.o: \
+  ../src/node_printer.cc ../src/node_printer.hpp ../src/macros.hh \
+  /Users/subekti/.node-gyp/10.15.1/include/node/node_version.h \
+  /Users/subekti/.node-gyp/10.15.1/include/node/node.h \
+  /Users/subekti/.node-gyp/10.15.1/include/node/v8.h \
+  /Users/subekti/.node-gyp/10.15.1/include/node/v8-version.h \
+  /Users/subekti/.node-gyp/10.15.1/include/node/v8config.h \
+  /Users/subekti/.node-gyp/10.15.1/include/node/v8-platform.h \
+  /Users/subekti/.node-gyp/10.15.1/include/node/node_buffer.h
+../src/node_printer.cc:
+../src/node_printer.hpp:
+../src/macros.hh:
+/Users/subekti/.node-gyp/10.15.1/include/node/node_version.h:
+/Users/subekti/.node-gyp/10.15.1/include/node/node.h:
+/Users/subekti/.node-gyp/10.15.1/include/node/v8.h:
+/Users/subekti/.node-gyp/10.15.1/include/node/v8-version.h:
+/Users/subekti/.node-gyp/10.15.1/include/node/v8config.h:
+/Users/subekti/.node-gyp/10.15.1/include/node/v8-platform.h:
+/Users/subekti/.node-gyp/10.15.1/include/node/node_buffer.h:
